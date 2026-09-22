74f5be04ac313df07c662063a3389bf03a80f034
Stephane Bance <bancestephane.r@gmail.com>
2026-09-22 08:03:27 -0200

    Exercice 5 Fini

--- src/net/lecnam/ussi2a/tp5/Exec.java
+++ src/net/lecnam/ussi2a/tp5/Exec.java
@@ -13,13 +13,22 @@ public class Exec {
 
        Rectangle rect1 = new Rectangle(25, 30, 10, 12);
        Rectangle rect2 = new Rectangle(55, 12, 65, 42);
-       Disque disc1 = new Disque(1, 1, 500);
+       Disque disc1 = new Disque(1, 1, 10);
        Disque disc2 = new Disque(30, 35, 10);
+
+       Triangle trig1 = new Triangle(20, 35, 56, 55, 78, 980);
+       Triangle trig2 = new Triangle(0, 35, 89, 78, 15, 51);
+       Triangle trig3 = new Triangle(29, 3, 6, 5, 45, 55);
+
+
 
        dessin.ajout(rect1);
        dessin.ajout(rect2);
        dessin.ajout(disc1);
        dessin.ajout(disc2);
+       dessin.ajout(trig1);
+       dessin.ajout(trig2);
+       dessin.ajout(trig3);
 
        System.out.println("Toutes les surfaces : " + dessin.retourneSurfaceTotale());
 
