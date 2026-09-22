b4cbfb94ee6ae4d3898f0f996e3dca89a8cf6f42
Stephane Bance <bancestephane.r@gmail.com>
2026-09-22 06:35:27 -0200

    Exercice 1 Fini

---
+++ src/net/lecnam/ussi2a/tp5/Disque.java
@@ -0,0 +1,34 @@
+package net.lecnam.ussi2a.tp5;
+
+public class Disque {
+
+    Point center;
+    Double rayon;
+
+    public Disque(Point center, Double rayon) {
+        this.center = center;
+        this.rayon = rayon;
+    }
+
+    public void translate(double x, double y) {
+        center.x += x;
+        center.y += y;
+    }
+
+    public double retourneSurface() {
+        return Math.PI * Math.pow(rayon, 2);
+    }
+
+    public double retournePerimetre() {
+        return 2 * Math.PI * rayon;
+    }
+
+    public boolean contient(Point ptn) {
+        return rayon >= center.retourneDistance(ptn);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("rayon : %f, centre : %s", rayon, center);
+    }
+}
