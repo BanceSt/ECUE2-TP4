5dc9a222f743e428d23a16088808231ebdc3d58f
Stephane Bance <bancestephane.r@gmail.com>
2026-09-22 08:29:16 -0200

    Exercice 6 Fini

--- src/net/lecnam/ussi2a/tp5/RectangleIncline.java
+++ src/net/lecnam/ussi2a/tp5/RectangleIncline.java
@@ -1,15 +1,42 @@
 package net.lecnam.ussi2a.tp5;
 
 public class RectangleIncline extends Rectangle{
-    double angles;
+    double angle;
 
-    public RectangleIncline(double longueur, double largeur, double x, double y, double angles) {
+    public RectangleIncline(double longueur, double largeur, double x, double y, double angle) {
         super(longueur, largeur, x, y);
-        this.angles = angles;
+        this.angle = angle;
     }
 
-    public RectangleIncline(double longueur, double largeur, Point point, double angles) {
+    public RectangleIncline(double longueur, double largeur, Point point, double angle) {
         super(longueur, largeur, point);
-        this.angles = angles;
+        this.angle = angle;
+    }
+
+    @Override
+    public boolean contient(Point point) {
+        double a = Math.toRadians(this.angle);
+        double dx = point.x - this.point.x;
+        double dy = point.y - this.point.y;
+        double rotx =  dx * Math.cos(a) + dy * Math.sin(a);
+        double roty = -dx * Math.sin(a) + dy * Math.cos(a);
+        return 0 <= rotx && rotx <= this.longueur
+                && 0 <= roty && roty <= this.largeur;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return super.equals(object) && (((RectangleIncline) object).angle == angle);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "longeur=" + longueur +
+                ", largeur=" + largeur +
+                ", point=" + point +
+                ", surface=" + this.retourneSurface() +
+                ", angle=" + angle +
+                '}';
     }
 }
